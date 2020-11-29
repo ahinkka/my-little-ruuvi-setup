58fc9150a26afe5c54d9776ec82377d6ed3729d1
Atte Hinkka <atte.hinkka@iki.fi>
2020-11-29 13:59:27 +0200

    First steps to enable more than just the last summary

--- measurement_summarizer.py
+++ measurement_summarizer.py
@@ -3,6 +3,12 @@ import contextlib
 import re
 import sqlite3
 import time
+import datetime as dt
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def table_name(measurement_type, period_secs):
@@ -31,7 +37,7 @@ def summarize_period_containing(conn, measurement_type, period_secs, containing_
         values = list(r[0] for r in conn.execute(f"SELECT {measurement_type} FROM measurement WHERE sensor = ? AND recorded_at >= ?", (sensor, period_start,)))
 
         if len(values) == 0:
-            print(f'No {measurement_type} values for {sensor} starting {period_start}, period {period_secs}')
+            logger.debug(f'No {measurement_type} values for {sensor} starting {period_start}, period {period_secs}')
             continue
 
         values.sort()
@@ -64,7 +70,7 @@ def summarize_single_period(conn, period_secs, epoch_secs_containing):
             summarize_period_containing(conn, measurement_type, period_secs, epoch_secs_containing)
 
 
-def main():
+def summarize_latest(args):
     with contextlib.closing(
             sqlite3.connect('measurements_backup.db',
                             detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)) as conn:
@@ -73,5 +79,39 @@ def main():
         summarize_single_period(conn, 86400, int(time.time()))
 
 
+def summarize_since(args):
+    with contextlib.closing(
+            sqlite3.connect('measurements_backup.db',
+                            detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)) as conn:
+        pass
+        # summarize_single_period(conn, 3600, int(time.time()))
+        # summarize_single_period(conn, 43200, int(time.time()))
+        # summarize_single_period(conn, 86400, int(time.time()))
+
 if __name__ == '__main__':
-    main()
+    import argparse
+
+    def valid_date(s):
+        format = "%Y-%m-%d"
+        try:
+            return dt.datetime.strptime(s, format)
+        except ValueError:
+            msg = f'invalid date: {s}, expected in format {format}'
+            raise argparse.ArgumentTypeError(msg)
+
+    parser = argparse.ArgumentParser(prog=__file__)
+
+    parser.add_argument('--debug', default=False, action='store_true')
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+    parser_a = subparsers.add_parser('summarize-latest')
+    parser_a.set_defaults(func=summarize_latest)
+    parser_b = subparsers.add_parser('summarize-since')
+    parser_b.add_argument('since', help='start date in YYYY-MM-DD format', type=valid_date)
+    parser_b.set_defaults(func=summarize_since)
+
+    args = parser.parse_args()
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    args.func(args)
